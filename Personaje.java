eeaad412d88b2f1c2100c478aaa533f762b67830
SergioSalido <ssgarcia2006@gmail.com>
2025-12-05 09:22:24 +0100

    Funcionalidad añadida

--- Personaje.java
+++ Personaje.java
@@ -1,17 +1,41 @@
 package pullrequest;
 
+import java.util.Scanner;
+
 public class Personaje {
- private String nom;
- private int dinero;
- public Personaje(String nom, int dinero) {
- this.nom = nom;
- this.dinero = dinero;
- }
- public void mostrarInfo() {
- System.out.println(nom + " tiene " + dinero + "$.");
- }
- public static void main(String[] args) {
- Personaje jugador = new Personaje("Trevor", 500);
- jugador.mostrarInfo();
- }
+    private String nom;
+    private int dinero;
+
+    public Personaje(String nom, int dinero) {
+        this.nom = nom;
+        this.dinero = dinero;
+    }
+
+    public void mostrarInfo() {
+        System.out.println(nom + " tiene " + dinero + "$.");
+    }
+
+    public static void main(String[] args) {
+        Personaje jugador = new Personaje("Trevor", 500);
+        jugador.mostrarInfo();
+
+        // Actualizamos el dinero del jugador llamando a la función
+        jugador.dinero = gastarDinero(jugador.dinero);
+
+        jugador.mostrarInfo(); // MOSTRARÁ EL DINERO TRAS GASTAR
+    }
+
+    public static int gastarDinero(int dineroActual) {
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Cuánto dinero quieres gastar?");
+        int dineroGastado = miScanner.nextInt();
+
+        int dineroRestante = dineroActual - dineroGastado;
+
+        System.out.println("Trevor gasta " + dineroGastado + "$ en armas.");
+        System.out.println("Se queda con: " + dineroRestante + "$");
+        miScanner.close();
+        return dineroRestante;
+    }
 }
+
